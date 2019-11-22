c883343bc862328540841a7feaa42088da27dfd6
manodya <manodyaabeysinghe7@gmail.com>
2019-11-22 13:50:30 +0530

    add all files

---
+++ UploadImageInDatabase/Controllers/Binaryreader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace UploadImageInDatabase.Controllers
+{
+    internal class Binaryreader
+    {
+        private Stream inputStream;
+
+        public Binaryreader(Stream inputStream)
+        {
+            this.inputStream = inputStream;
+        }
+
+        internal string ReadBytes(Stream inputStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UploadImageInDatabase/Controllers/HomeController.cs
+++ UploadImageInDatabase/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ namespace UploadImageInDatabase.Controllers
 {
     public class HomeController : Controller
     {
+        private string imagebyte;
+
         public ActionResult Index()
         {
             MVCImageDBEntities db = new MVCImageDBEntities();
@@ -16,18 +18,34 @@ namespace UploadImageInDatabase.Controllers
             return View();
         }
 
-        //public JsonResult ImageUpload(ImageViewModel model)
-        //{
-        //    MVCImageDBEntities db = new MVCImageDBEntities();
-        //    int imgId = 0;
-        //    var file = model.ImageFile;
-        //        byte[] Imagebyte = null;
-        //    if (file != null)
-        //    {
-        //        file.SaveAs(Server.MapPath("/UploadImage" + file.FileName));
-        //    }
-        //}
+        public JsonResult ImageUpload(ImageViewModel model)
+        {
+            MVCImageDBEntities db = new MVCImageDBEntities();
+            int imgId = 0;
+            var file = model.ImageFile;
+            byte[] Imagebyte = null;
+            if (file != null)
+            {
+                file.SaveAs(Server.MapPath("/UploadImage" + file.FileName));
+                Binaryreader reader = new Binaryreader(file.InputStream);
+                imagebyte = reader.ReadBytes(file.InputStream);
+                StoreImage img = new StoreImage();
+                img.ImageTitle = file.FileName;
+                img.ImageByte = imagebyte;
+                img.ImagePath = "/Upload/" + file.FileName;
+                db.SaveChanges();
+                imgId = img.ImageId;
+            }
+            return Json(imgId, JsonRequestBehavior.AllowGet);
+        }
+public ActionResult DisplayingImage(int imgID)
+        {
+            MVCImageDBEntities db = new MVCImageDBEntities();
 
+            var img = db.StoreImages.SingleOrDefault(x => x.ImageId == imgID);
+
+            return File(img.ImageByte, "image/jpg");
+        }
 
     }
 }
